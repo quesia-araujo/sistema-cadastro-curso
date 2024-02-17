af9f366701407f188a65f30418a9e6b33ec4d7ad
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-02-17 17:06:54 -0400

    first commit

---
+++ base/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class Cadastro(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField(max_length=75)
+    senha = models.CharField(max_length=50)
+    data = models.DateTimeField(auto_now_add=True)
