2f1871d37dc9883529361231a06bec45ab3680b8
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-03-11 14:10:10 -0400

    serializers

--- rest_api/urls.py
+++ rest_api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from rest_api.views import hello_word
+from rest_framework.routers import SimpleRouter
+from rest_api.views import CursoModelViewSet
 
 app_name = 'rest_api'
+router = SimpleRouter(trailing_slash=False) # falso - não vai barra ao final da url
+router.register('curso', CursoModelViewSet) # argumentos - prefixo e qual view
+
 urlpatterns = [
     path('hello_word', hello_word, name='hello_world_api')
 ]
+
+urlpatterns += router.urls
