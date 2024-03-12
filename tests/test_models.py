56bf5fdf425e93ffae1d7d7eeff9c3bc4484d2da
quesia-araujo <67472987+quesia-araujo@users.noreply.github.com>
2024-03-12 14:08:27 -0400

    feat: adiciona fixture ao teste de model

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,14 +3,19 @@ from model_bakery import baker
 from cursos.models import Curso
 import pytest
 
-@pytest.mark.django_db # acesso ao banco de dados
-def test_srt_deve_retornar_string_formatada():
+@pytest.fixture
+def curso():
     curso = baker.make(
         Curso,
         titulo = 'Java',
         data_do_curso = date.today(),
         carga_horaria = '40'
     )
+    return curso
+
+
+@pytest.mark.django_db # acesso ao banco de dados
+def test_str_deve_retornar_string(curso):
     assert str(curso) == 'Java: 2024-03-12 - 40' # Data do dia atual
 
 #def test_config():
